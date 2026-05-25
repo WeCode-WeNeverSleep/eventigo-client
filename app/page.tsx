6e1bfd77a4a44db253069d5667a648b9efab245f
mayahNeko <mayahibela@gmail.com>
2026-05-25 14:58:35 +0300

    feat: creat event page and implement QuestionForm, SpeakerComponent in it

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,11 @@
 import { ModeToggle } from "@/components/theme-toggle";
-import QuestionForm from "@/components/question-component/QuestionForm";
-import SpeakerComponent from "@/components/SpeakerComponent/SpeakerComponent";
+import EventPage from "./event/page";
 
 export default function Home() {
   return (
     <>
       <ModeToggle />
-      <div className="flex flex-col md:flex-row px-5">
-        <div className="md:w-2/3 w-full">
-          <QuestionForm />
-        </div>
-
-        <div className="md:w-1/3 w-full">
-          <SpeakerComponent />
-        </div>
-      </div>
+      <EventPage />
     </>
   );
 }
